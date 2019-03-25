30fd967cee198c963153d81ec3fbd656091fbbc1
andrewjahn <48071253+andrewjahn@users.noreply.github.com>
2019-03-25 13:48:35 -0400

    Update createSphere.sh

--- createSphere.sh
+++ createSphere.sh
@@ -5,12 +5,12 @@
 display_usage() {
 	echo "$(basename $0) [sphere radius] [master dataset] [x] [y] [z] [ROI name]"
 	echo "This script creates a sphere for ROI analysis. It requires 6 arguments:
-		1) The size of the radius of the sphere;
-		2) A master dataset, which determines the dimensions and voxel resolution of the ROI [I recommend using the 			errts file created by 3dDeconvolve];
+		1) The size of the radius of the sphere (in millimeters);
+		2) A master dataset, which determines the dimensions and voxel resolution of the ROI [I recommend using the errts file created by 3dDeconvolve];
 		3-5) X, Y, and Z coordinates (assumed to be MNI and in LPI orientation);
 		6) A label for the ROI"
 		
-		This command also creates a time series from the ROI, labeled [ROI name]_TimeSeries.txt
+		This command also extracts a time series from the ROI, labeled [ROI name]_TimeSeries.txt, and performs a functinal connectivity analysis on that time series.
 	}
 
 	if [ $# -le 5 ]
@@ -34,9 +34,9 @@ OUTPUT=$6
 echo $X $Y $Z | 3dUndump -prefix $OUTPUT -srad $SRAD -orient LPI -master $MASTER -xyz -
 
 
-#######################################
-# Extract the timeseries from the ROI #
-#######################################
+########################################
+# Extract the time series from the ROI #
+########################################
 
 3dmaskave -quiet -mask ${OUTPUT}+tlrc $MASTER > ${OUTPUT}_TimeSeries.txt
 
